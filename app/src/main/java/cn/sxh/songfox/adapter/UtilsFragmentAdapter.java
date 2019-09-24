0e7074b2a9896bed3b36e76efb3b1648fe7dd453
snowFox <1215167396@qq.com>
2019-09-24 10:39:15 +0800

    提交代码修改

---
+++ app/src/main/java/cn/sxh/songfox/adapter/UtilsFragmentAdapter.java
@@ -0,0 +1,89 @@
+package cn.sxh.songfox.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cn.sxh.songfox.R;
+
+/**
+ * @package-name: cn.sxh.songfox.adapter
+ * @auther:snowFox
+ * @Email:snowTigersong@gmail.com
+ * @time: 2019/8/23 0023 : 14 :18
+ * @project-name: songFox
+ */
+public class UtilsFragmentAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private List<String> list;
+    private LayoutInflater inflater;
+
+    public UtilsFragmentAdapter(Context context, List<String> list) {
+        this.mContext = context;
+        this.list = list;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return list == null ? 0 : list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.tools_fragment_list_item, parent, false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        initInfo(holder, position);
+        initUIListener(holder, position);
+        return convertView;
+    }
+
+    private void initInfo(ViewHolder holder, int position) {
+        holder.mLinearLayout.setOnClickListener(view ->listener.onLinearLayoutClick(holder,position));
+    }
+
+    private void initUIListener(ViewHolder holder, int position) {
+        holder.title_name.setText(list.get(position));
+    }
+
+    public class ViewHolder{
+        private TextView title_name;
+        private LinearLayout mLinearLayout;
+
+        ViewHolder(View view) {
+            title_name = view.findViewById(R.id.tools_fragment_item_text);
+            mLinearLayout = view.findViewById(R.id.all_fragment_linearLayout);
+        }
+    }
+
+    private OnLinearLayoutListener listener;
+    public interface OnLinearLayoutListener{
+        void onLinearLayoutClick(ViewHolder holder, int position);
+    }
+    public void setOnLinearLayoutListener(OnLinearLayoutListener layoutListener) {
+        this.listener = layoutListener;
+    }
+}
--- app/src/main/java/cn/sxh/songfox/mvp/UI/fragment/UtilsFragment.java
+++ app/src/main/java/cn/sxh/songfox/mvp/UI/fragment/UtilsFragment.java
@@ -1,13 +1,16 @@
 package cn.sxh.songfox.mvp.UI.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import cn.sxh.songfox.AppContext;
 import cn.sxh.songfox.R;
+import cn.sxh.songfox.adapter.UtilsFragmentAdapter;
 import cn.sxh.songfox.base.BaseFragment;
 
 /**
@@ -19,6 +22,10 @@ import cn.sxh.songfox.base.BaseFragment;
  */
 public class UtilsFragment extends BaseFragment {
 
+    private static final String TAG = "UtilsFragment";
+    private ListView mListView;
+    private List<String> list = new ArrayList<>();
+    private UtilsFragmentAdapter fragmentAdapter;
 
     @Override
     protected int getContentView() {
@@ -27,11 +34,20 @@ public class UtilsFragment extends BaseFragment {
 
     @Override
     protected void initUI(View view) {
-
+        mListView = view.findViewById(R.id.tools_fragment_listView);
     }
 
     @Override
     protected void initData() {
+        list = Arrays.asList(AppContext.getInstance().
+                getResources().getStringArray(R.array.tools_fragment_item));
+        fragmentAdapter = new UtilsFragmentAdapter(getContext(),list);
+        mListView.setAdapter(fragmentAdapter);
+        fragmentAdapter.setOnLinearLayoutListener((holder, position)
+                -> gotoActivity(position));
+    }
 
+    private void gotoActivity(int position) {
+        Log.e(TAG, "--------->" + position);
     }
 }
