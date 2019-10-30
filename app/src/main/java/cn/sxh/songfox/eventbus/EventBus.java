88bcf57fdcc7fea4cac45571c72915a9d4636681
snowFox <1215167396@qq.com>
2019-10-30 21:02:31 +0800

    Event bus code

---
+++ app/src/main/java/cn/sxh/songfox/eventbus/EventBus.java
@@ -0,0 +1,11 @@
+package cn.sxh.songfox.eventbus;
+
+/**
+ * @package-name: cn.sxh.songfox.eventbus
+ * @auther:snowFox
+ * @Email:snowTigersong@gmail.com
+ * @time: 2019/10/30 0030 : 20 :30
+ * @project-name: songFox
+ */
+public class EventBus {
+}
