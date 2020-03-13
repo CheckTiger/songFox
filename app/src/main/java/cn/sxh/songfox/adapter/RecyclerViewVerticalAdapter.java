dc1af1bc439e065bd502a06628695465cc0f1494
snow <1215167396@qq.com>
2020-03-14 00:53:54 +0800

    recyclerView的使用

---
+++ app/src/main/java/cn/sxh/songfox/adapter/RecyclerViewVerticalAdapter.java
@@ -0,0 +1,58 @@
+package cn.sxh.songfox.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.sxh.songfox.R;
+
+public class RecyclerViewVerticalAdapter extends RecyclerView.Adapter<RecyclerViewVerticalAdapter.VerticalViewHolder> {
+
+    private Context mContext;
+    private List<String> mList = new ArrayList<>();
+
+    public RecyclerViewVerticalAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public void setList(List<String> list) {
+        this.mList = list;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public VerticalViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.technology_fragment_vertical_recycleview_item, parent, false);
+        return new VerticalViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VerticalViewHolder holder, int position) {
+        holder.mTextView.setText(mList.get(position));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mList == null ? 0 : mList.size();
+    }
+
+    public class VerticalViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mTextView;
+        public VerticalViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTextView = itemView.findViewById(R.id.tv);
+        }
+    }
+
+}
--- app/src/main/java/cn/sxh/songfox/mvp/ui/fragment/TechnologyFragment.java
+++ app/src/main/java/cn/sxh/songfox/mvp/ui/fragment/TechnologyFragment.java
@@ -1,16 +1,21 @@
 package cn.sxh.songfox.mvp.ui.fragment;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.os.Build;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import cn.sxh.songfox.AppContext;
+import cn.sxh.songfox.R;
+import cn.sxh.songfox.adapter.RecyclerViewVerticalAdapter;
 import cn.sxh.songfox.base.BaseFragment;
-
-import static cn.sxh.songfox.util.AppUtil.getContext;
+import cn.sxh.songfox.util.AppUtil;
 
 /**
  * @package-name: cn.sxh.songfox.mvp.UI.fragment
@@ -19,29 +24,34 @@ import static cn.sxh.songfox.util.AppUtil.getContext;
  * @time: 2019/7/3 0003 : 19 :39
  * @project-name: songFox
  */
+
+@RequiresApi(api = Build.VERSION_CODES.P)
 public class TechnologyFragment extends BaseFragment {
 
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        TextView textView = new TextView(getContext());
-        textView.setText("技术天地");
-        return textView;
-    }
+    private RecyclerView mRecyclerView;
+    private RecyclerViewVerticalAdapter recyclerViewVerticalAdapter;
 
     @Override
     protected int getContentView() {
-        return 0;
+        return R.layout.technology_fragment_layout;
     }
 
     @Override
     protected void initUI(View view) {
-
+        mRecyclerView = view.findViewById(R.id.Technology_fragment_listView);
+        recyclerViewVerticalAdapter = new RecyclerViewVerticalAdapter(AppUtil.getContext());
     }
 
     @Override
     protected void initData() {
-
+        List<String> list = Arrays.asList(AppContext.getInstance().
+                getResources().getStringArray(R.array.question_fragment_item));
+        LinearLayoutManager managerVertical = new LinearLayoutManager(AppUtil.getContext());
+        managerVertical.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(managerVertical);
+        mRecyclerView.setAdapter(recyclerViewVerticalAdapter);
+        recyclerViewVerticalAdapter.setList(list);
     }
 }
