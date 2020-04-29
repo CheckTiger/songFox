208590d8310c0b7c3d87b9ba65505b44ff5c281a
snowFox <1215167396@qq.com>
2020-04-29 21:11:13 +0800

    @songxuehu：commit: 提交OkHttp框架学习代码

---
+++ sxh-base/src/main/java/cn/sxh/base/okhttp/Request.java
@@ -0,0 +1,17 @@
+package cn.sxh.base.okhttp;
+
+/**
+ * @package-name: cn.sxh.base.okhttp
+ * @auther:snowFox
+ * @Email:snowTigersong@gmail.com
+ * @time: 2020/4/29 0029 : 21 :00
+ * @project-name: songFox
+ */
+public final class Request {
+
+    final HttpUrl url;
+    final String method;
+    final Headers headers;
+    final RequestBody body;
+    final Object tag;
+}
