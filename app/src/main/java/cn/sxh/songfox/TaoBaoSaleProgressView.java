79033a738e3e22e6f318781e888b77cb6fd22a74
snow <1215167396@qq.com>
2021-04-07 23:20:31 +0800

    【view学习】商品抢购进度条

---
+++ app/src/main/java/cn/sxh/songfox/TaoBaoSaleProgressView.java
@@ -0,0 +1,242 @@
+package cn.sxh.songfox;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import java.text.DecimalFormat;
+
+
+public class TaoBaoSaleProgressView extends View {
+    private int totalCount;//商品总数
+    private int currentCount;//当前卖出数量
+    private int progressCount;//动画所需要的进度
+    private float scale;//售出比列
+    private int sideColor;//边框颜色
+    private int textColor;//文字颜色
+    private float sideWidth;//边框粗细
+    private Paint sidePaint;//边框所在的矩形
+
+    private RectF bgRectF;
+    private float radius;
+    private int width;
+    private int height;
+    private PorterDuffXfermode mPorterDuffXfermode;
+    private Paint srcPaint;
+    private Bitmap fgSrc;
+    private Bitmap bgSrc;
+
+    private String nearOverText;
+    private String overText;
+    private float textSize;
+
+    private Paint textPaint;
+    private float nearOverTextWidth;
+    private float overTextWidth;
+    private float baseLineY;
+    private Bitmap bgBitmap;
+    private boolean isNeedAnim;
+
+    public TaoBaoSaleProgressView(Context context) {
+        this(context,null);
+    }
+
+    public TaoBaoSaleProgressView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initAttrs(context,attrs);
+    }
+
+    private void initAttrs(Context context, AttributeSet attrs) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TaoBaoSaleProgressView);
+        sideColor = a.getColor(R.styleable.TaoBaoSaleProgressView_sideColor,0xffff3c32);
+        textColor = a.getColor(R.styleable.TaoBaoSaleProgressView_textColor,0xffff3c32);
+        sideWidth = a.getDimension(R.styleable.TaoBaoSaleProgressView_sideWidth,dp2px(2));
+        overText = a.getString(R.styleable.TaoBaoSaleProgressView_overText);
+        nearOverText = a.getString(R.styleable.TaoBaoSaleProgressView_nearOverText);
+        textSize = a.getDimension(R.styleable.TaoBaoSaleProgressView_textSize,sp2px(16));
+        isNeedAnim = a.getBoolean(R.styleable.TaoBaoSaleProgressView_isNeedAnim,true);
+        a.recycle();
+        initPaint();
+    }
+
+    private void initPaint() {
+        sidePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        sidePaint.setStyle(Paint.Style.STROKE);
+        sidePaint.setStrokeWidth(sideWidth);
+        sidePaint.setColor(sideColor);
+
+        srcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setTextSize(textSize);
+
+        mPorterDuffXfermode = new PorterDuffXfermode(PorterDuff.Mode.SRC_IN);
+        nearOverTextWidth = textPaint.measureText(nearOverText);
+        overTextWidth = textPaint.measureText(overText);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        width = getMeasuredWidth();
+        height = getMeasuredHeight();
+
+        radius = height / 2.0f;
+
+        if (bgRectF == null) {
+            bgRectF = new RectF(sideWidth, sideWidth, width - sideWidth, height - sideWidth);
+        }
+
+        if (baseLineY == 0.0f) {
+            Paint.FontMetricsInt fm = textPaint.getFontMetricsInt();
+            baseLineY = height / 2 - (fm.descent / 2 + fm.ascent / 2);
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (!isNeedAnim) {
+            progressCount = currentCount;
+        }
+        if (totalCount == 0) {
+            scale = 0.0f;
+        } else {
+            scale = Float.parseFloat(new DecimalFormat("0.00").format((float) progressCount / (float) totalCount));
+        }
+        drawSide(canvas);
+        drawBg(canvas);
+        drawFg(canvas);
+        drawText(canvas);
+        //这里是为了演示动画方便，实际开发中进度只会增加
+        if(progressCount!=currentCount){
+            if(progressCount<currentCount){
+                progressCount++;
+            }else{
+                progressCount--;
+            }
+            postInvalidate();
+        }
+    }
+
+    /**
+     * 绘制背景边框
+     * @param canvas
+     */
+    private void drawSide(Canvas canvas) {
+        canvas.drawRoundRect(bgRectF, radius, radius, sidePaint);
+//        drawBg(canvas);
+    }
+
+    /**
+     * 绘制背景
+     * @param canvas
+     */
+    private void drawBg(Canvas canvas) {
+        if (bgBitmap == null) {
+            bgBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        }
+        Canvas bgCanvas = new Canvas(bgBitmap);
+        if (bgSrc == null) {
+            bgSrc = BitmapFactory.decodeResource(getResources(), cn.sxh.resource.R.mipmap.bg);
+        }
+        bgCanvas.drawRoundRect(bgRectF, radius, radius, srcPaint);
+
+        srcPaint.setXfermode(mPorterDuffXfermode);
+        bgCanvas.drawBitmap(bgSrc, null, bgRectF, srcPaint);
+
+        canvas.drawBitmap(bgBitmap, 0, 0, null);
+        srcPaint.setXfermode(null);
+//        drawFg(canvas);
+
+    }
+
+    /**
+     * 绘制进度条
+     * @param canvas
+     */
+    private void drawFg(Canvas canvas) {
+        if (scale == 0.0f) {
+            return;
+        }
+        Bitmap fgBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas fgCanvas = new Canvas(fgBitmap);
+        if (fgSrc == null) {
+            fgSrc = BitmapFactory.decodeResource(getResources(),cn.sxh.resource.R.mipmap.fg);
+        }
+        fgCanvas.drawRoundRect(
+                new RectF(sideWidth, sideWidth, (width - sideWidth) * scale, height - sideWidth),
+                radius, radius, srcPaint);
+
+        srcPaint.setXfermode(mPorterDuffXfermode);
+        fgCanvas.drawBitmap(fgSrc, null, bgRectF, srcPaint);
+
+        canvas.drawBitmap(fgBitmap, 0, 0, null);
+        srcPaint.setXfermode(null);
+//        drawText(canvas);
+    }
+
+    /**
+     * 绘制文字信息
+     * @param canvas
+     */
+    private void drawText(Canvas canvas) {
+        String scaleText = new DecimalFormat("#%").format(scale);
+        String saleText = String.format("已抢%s件", progressCount);
+
+        float scaleTextWidth = textPaint.measureText(scaleText);
+
+        Bitmap textBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas textCanvas = new Canvas(textBitmap);
+        textPaint.setColor(textColor);
+
+        if (scale < 0.8f) {
+            textCanvas.drawText(saleText, dp2px(10), baseLineY, textPaint);
+            textCanvas.drawText(scaleText, width - scaleTextWidth - dp2px(10), baseLineY, textPaint);
+        } else if (scale < 1.0f) {
+            textCanvas.drawText(nearOverText, width / 2 - nearOverTextWidth / 2, baseLineY, textPaint);
+            textCanvas.drawText(scaleText, width - scaleTextWidth - dp2px(10), baseLineY, textPaint);
+        } else {
+            textCanvas.drawText(overText, width / 2 - overTextWidth / 2, baseLineY, textPaint);
+        }
+
+        textPaint.setXfermode(mPorterDuffXfermode);
+        textPaint.setColor(Color.WHITE);
+        textCanvas.drawRoundRect(
+                new RectF(sideWidth, sideWidth, (width - sideWidth) * scale, height - sideWidth),
+                radius, radius, textPaint);
+        canvas.drawBitmap(textBitmap, 0, 0, null);
+        textPaint.setXfermode(null);
+    }
+
+    private int dp2px(float dpValue) {
+        float scale = getContext().getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    private int sp2px(float spValue) {
+        float scale = getContext().getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * scale + 0.5f);
+    }
+
+    public void setTotalAndCurrentCount(int totalCount, int currentCount) {
+        this.totalCount = totalCount;
+        if (currentCount > totalCount) {
+            currentCount = totalCount;
+        }
+        this.currentCount = currentCount;
+        postInvalidate();
+    }
+}
