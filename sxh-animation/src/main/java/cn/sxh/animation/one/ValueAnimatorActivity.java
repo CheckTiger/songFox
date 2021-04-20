a557f87611b033d1061b4c8981034a8c54a68b48
songxuehu <1215167396@qq.com>
2021-04-20 15:57:21 +0800

    【动画学习】: 属性动画混合使用

--- sxh-animation/src/main/java/cn/sxh/animation/one/ValueAnimatorActivity.java
+++ sxh-animation/src/main/java/cn/sxh/animation/one/ValueAnimatorActivity.java
@@ -8,7 +8,9 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.BounceInterpolator;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +23,13 @@ public class ValueAnimatorActivity extends AppCompatActivity implements View.OnC
 
     private AvatarView mAvatarView;
     private TextView start,show, cancel;
+    private Button mMenuButton;
+    private Button mItemButton1;
+    private Button mItemButton2;
+    private Button mItemButton3;
+    private Button mItemButton4;
+    private Button mItemButton5;
+    private boolean mIsMenuOpen = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +42,25 @@ public class ValueAnimatorActivity extends AppCompatActivity implements View.OnC
         start = findViewById(R.id.start);
         show = findViewById(R.id.show);
         cancel = findViewById(R.id.cancel);
+
+        mMenuButton = (Button) findViewById(R.id.menu);
+        mMenuButton.setOnClickListener(this);
+
+        mItemButton1 = (Button) findViewById(R.id.item1);
+        mItemButton1.setOnClickListener(this);
+
+        mItemButton2 = (Button) findViewById(R.id.item2);
+        mItemButton2.setOnClickListener(this);
+
+        mItemButton3 = (Button) findViewById(R.id.item3);
+        mItemButton3.setOnClickListener(this);
+
+        mItemButton4 = (Button) findViewById(R.id.item4);
+        mItemButton4.setOnClickListener(this);
+
+        mItemButton5 = (Button) findViewById(R.id.item5);
+        mItemButton5.setOnClickListener(this);
+
         initListener();
     }
 
@@ -52,6 +80,24 @@ public class ValueAnimatorActivity extends AppCompatActivity implements View.OnC
             initValueAnimation();
         } else if (id == R.id.cancel) {
             playWithValueAnimation();
+        } else if (v == mMenuButton) {
+            if (!mIsMenuOpen) {
+                mIsMenuOpen = true;
+                doAnimateOpen(mItemButton1, 0, 5, 300);
+                doAnimateOpen(mItemButton2, 1, 5, 300);
+                doAnimateOpen(mItemButton3, 2, 5, 300);
+                doAnimateOpen(mItemButton4, 3, 5, 300);
+                doAnimateOpen(mItemButton5, 4, 5, 300);
+            } else {
+                mIsMenuOpen = false;
+                doAnimateClose(mItemButton1, 0, 5, 300);
+                doAnimateClose(mItemButton2, 1, 5, 300);
+                doAnimateClose(mItemButton3, 2, 5, 300);
+                doAnimateClose(mItemButton4, 3, 5, 300);
+                doAnimateClose(mItemButton5, 4, 5, 300);
+            }
+        } else {
+            Toast.makeText(this, "你点击了" + v, Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -100,6 +146,49 @@ public class ValueAnimatorActivity extends AppCompatActivity implements View.OnC
         animatorSet.setDuration(2000);
         animatorSet.start();
     }
+
+    private void doAnimateOpen(View view, int index, int total, int radius) {
+        if (view.getVisibility() != View.VISIBLE) {
+            view.setVisibility(View.VISIBLE);
+        }
+        double degree = Math.toRadians(90)/(total - 1) * index;
+        int translationX = -(int) (radius * Math.sin(degree));
+        int translationY = -(int) (radius * Math.cos(degree));
+
+        AnimatorSet set = new AnimatorSet();
+        //包含平移、缩放和透明度动画
+        set.playTogether(
+                ObjectAnimator.ofFloat(view, "translationX", 0, translationX),
+                ObjectAnimator.ofFloat(view, "translationY", 0, translationY),
+                ObjectAnimator.ofFloat(view, "scaleX", 0f, 1f),
+                ObjectAnimator.ofFloat(view, "scaleY", 0f, 1f),
+                ObjectAnimator.ofFloat(view, "alpha", 0f, 1));
+        //动画周期为500ms
+        set.setDuration(1 * 500).start();
+    }
+
+
+    private void doAnimateClose(final View view, int index, int total,
+                                int radius) {
+        if (view.getVisibility() != View.VISIBLE) {
+            view.setVisibility(View.VISIBLE);
+        }
+        double degree = Math.PI * index / ((total - 1) * 2);
+        int translationX = -(int) (radius * Math.sin(degree));
+        int translationY = -(int) (radius * Math.cos(degree));
+        AnimatorSet set = new AnimatorSet();
+        //包含平移、缩放和透明度动画
+        set.playTogether(
+                ObjectAnimator.ofFloat(view, "translationX", translationX, 1),
+                ObjectAnimator.ofFloat(view, "translationY", translationY, 1),
+                ObjectAnimator.ofFloat(view, "scaleX", 1f, 0.1f),
+                ObjectAnimator.ofFloat(view, "scaleY", 1f, 0.1f),
+                ObjectAnimator.ofFloat(view, "alpha", 1f, 0.1f));
+
+        set.setDuration(500).start();
+    }
+
+
 
 
 }
