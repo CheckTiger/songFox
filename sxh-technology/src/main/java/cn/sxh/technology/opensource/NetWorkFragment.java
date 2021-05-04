b42e5234d05ae05588aae0c5bb240068112f9675
snow <1215167396@qq.com>
2021-05-04 22:33:22 +0800

    【网络模块】:添加网路模块学习

---
+++ sxh-technology/src/main/java/cn/sxh/technology/opensource/NetWorkFragment.java
@@ -0,0 +1,33 @@
+package cn.sxh.technology.opensource;
+
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.sxh.base.BaseFragment;
+import cn.sxh.technology.R;
+
+public class NetWorkFragment extends BaseFragment {
+
+    private static final String TAG = "QuestionFragment";
+    private ListView mListView;
+    private List<String> list = new ArrayList<>();
+    private UtilsFragmentAdapter fragmentAdapter;
+
+    @Override
+    protected int getContentView() {
+        return R.layout.network_fragment_layout;
+    }
+
+    @Override
+    protected void initUI(View view) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
